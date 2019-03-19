2e33ff798ea8244776b87be82415883467b4cbc9
bin_mobile_comp <nthanhphong89@gmail.com>
2019-03-19 20:46:14 +0700

    dsdsd

--- Code/app/src/main/java/vn/edu/httpgreenacademy/vuivc/MainActivity.java
+++ Code/app/src/main/java/vn/edu/httpgreenacademy/vuivc/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String myname = "My NAME: CƯỜNG" + "\n" + "Class: Android";
         //phong
-
+ 
         String duy = "abcnd";
 
 
