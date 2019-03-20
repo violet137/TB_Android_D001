72b142af51001a2454c802064d7e9742d0ed25c8
Trần Thị Bích Hồng <tranthibichhong90proweb@gmail.com>
2019-03-20 19:05:04 +0700

    Update MainActivity

--- Code/app/src/main/java/vn/edu/httpgreenacademy/vuivc/MainActivity.java
+++ Code/app/src/main/java/vn/edu/httpgreenacademy/vuivc/MainActivity.java
@@ -9,12 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String myname = "My NAME: CƯỜNG" + "\n" + "Class: Android";
-        //phong
-
-        
-        String duy = "abcnd";
-
-
     }
 }
