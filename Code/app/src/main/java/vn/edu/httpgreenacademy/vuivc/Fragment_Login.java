d795dd0aa9b22f7bf83c48bd8c789f42b811a877
duy-nghia <cakestore.java@gmail.com>
2019-04-06 18:55:56 +0700

    merge code login

--- Code/app/src/main/java/vn/edu/httpgreenacademy/vuivc/Fragment_Login.java
+++ Code/app/src/main/java/vn/edu/httpgreenacademy/vuivc/Fragment_Login.java
@@ -5,7 +5,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,20 +12,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.facebook.AccessToken;
+
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
-public class Fragment_Login extends Fragment implements View.OnClickListener,Login_Status{
+public class Fragment_Login extends Fragment implements View.OnClickListener, Login_Status {
     TextView Txt_Error;
     EditText Ed_UserName, Ed_Password;
-    Button Bt_Login,Bt_Register,Bt_Home_Login;
+    Button Bt_Login, Bt_Register, Bt_Home_Login;
     LoginButton btn_login_facebook;
     CallbackManager callbackManager;
     int KIEM_TRA_DANG_NHAP;
@@ -54,65 +50,54 @@ public class Fragment_Login extends Fragment implements View.OnClickListener,Log
         AccessToken accessToken = AccessToken.getCurrentAccessToken();
         boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
         LoginManager.getInstance().logOut();*/
-        
+
         return view;
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId())
-        {
+        switch (v.getId()) {
             case R.id.BT_Login_Login:
-                Login_ASyctask login_aSyctask = new Login_ASyctask( this);
-                login_aSyctask.execute(Ed_UserName.getText().toString(),Ed_Password.getText().toString());
+                Login_ASyctask login_aSyctask = new Login_ASyctask(this);
+                login_aSyctask.execute(Ed_UserName.getText().toString(), Ed_Password.getText().toString());
                 break;
             case R.id.BT_Register_Login:
                 Fragment_Register fragment_register = new Fragment_Register();
                 FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                fragmentManager.beginTransaction().replace(android.R.id.content,fragment_register).addToBackStack("s").commit();
+                fragmentManager.beginTransaction().replace(android.R.id.content, fragment_register).addToBackStack("s").commit();
                 break;
             case R.id.Bt_Home_Login:
                 Fragment_Home fragment_home = new Fragment_Home();
                 FragmentManager fragmentManager1 = getActivity().getSupportFragmentManager();
-                fragmentManager1.beginTransaction().replace(android.R.id.content,fragment_home).commit();
+                fragmentManager1.beginTransaction().replace(android.R.id.content, fragment_home).commit();
                 break;
         }
     }
 
     @Override
     public void LoginStatus(Boolean isThanhCong) {
-        if(isThanhCong)
-        {
+        if (isThanhCong) {
             Txt_Error.setText(getResources().getString(R.string.Thanhcong));
+        } else {
+            callbackManager = CallbackManager.Factory.create(); // xử lý các phản hồi đăng nhập
+            btn_login_facebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                @Override
+                public void onSuccess(LoginResult loginResult) {
+                    String token = loginResult.getAccessToken().getToken();
+                    Toast.makeText(getContext(), "Success", Toast.LENGTH_LONG).show();
+
+                }
+
+                @Override
+                public void onCancel() {
+
+                }
+
+                @Override
+                public void onError(FacebookException error) {
+
+                }
+            });
         }
-        else
-        {
-        callbackManager = CallbackManager.Factory.create(); // xử lý các phản hồi đăng nhập
-        btn_login_facebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                String token = loginResult.getAccessToken().getToken();
-                Toast.makeText(getContext(), "Success", Toast.LENGTH_LONG).show();
-
-            }
-
-            @Override
-            public void onCancel() {
-
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-
-            }
-        });
-
-        return view;
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
