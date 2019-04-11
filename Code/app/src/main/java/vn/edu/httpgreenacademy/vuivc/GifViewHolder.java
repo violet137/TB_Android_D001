3af0b796d15c96e9d6bb46cf8487b5895164981c
tungxuan15419991 <tungxuan15419991@gmail.com>
2019-04-11 19:50:11 +0700

    GifView_2

--- Code/app/src/main/java/vn/edu/httpgreenacademy/vuivc/GifViewHolder.java
+++ Code/app/src/main/java/vn/edu/httpgreenacademy/vuivc/GifViewHolder.java
@@ -1,6 +1,5 @@
 package vn.edu.httpgreenacademy.vuivc;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -12,7 +11,7 @@ import com.bumptech.glide.Glide;
 public class GifViewHolder extends RecyclerView.ViewHolder {
     Activity activity;
 
-    View clickView;
+//    View clickView;
     ImageView previewImage;
     GifView gifView;
 
@@ -21,34 +20,51 @@ public class GifViewHolder extends RecyclerView.ViewHolder {
 
         this.activity = activity;
 
-        clickView = itemView.findViewById(R.id.touch_effect_item);
+//        clickView = itemView.findViewById(R.id.touch_effect_item);
         previewImage = (ImageView) itemView.findViewById(R.id.preview_image_gif_item);
         gifView = (GifView) itemView.findViewById(R.id.gif_view_item);
     }
 
     void bind(final Gif gif) {
-        Glide.with(itemView.getContext()).load(gif.getPreviewImageUrl()).centerCrop().into(previewImage);
+        Glide.with(itemView.getContext()).load(gif.getPreviewImageUrl())..into(previewImage);
         gifView.setVisibility(View.VISIBLE);
         gifView.start(gif.getPreviewMp4Url());
 
-        clickView.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("NewApi")
+        gifView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (gifView.getVisibility() == View.VISIBLE) {
-                    gifView.release();
-                    gifView.setVisibility(View.GONE);
-                } else {
-                    gifView.setVisibility(View.VISIBLE);
-                    gifView.start(gif.getPreviewMp4Url());
-                }
+                gifView.release();
+                gifView.setVisibility(View.GONE);
             }
         });
+
+        previewImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gifView.setVisibility(View.VISIBLE);
+                gifView.start(gif.getPreviewMp4Url());
+            }
+        });
+//        clickView.setOnClickListener(new View.OnClickListener() {
+//            @SuppressLint("NewApi")
+//            @Override
+//            public void onClick(View view) {
+//                if (gifView.getVisibility() == View.VISIBLE) {
+//                    gifView.release();
+//                    gifView.setVisibility(View.GONE);
+//                } else {
+//                    gifView.setVisibility(View.VISIBLE);
+//                    gifView.start(gif.getPreviewMp4Url());
+//                }
+//            }
+//        });
     }
 
     public void stopPlayback() {
         if (gifView.getVisibility() == View.VISIBLE) {
-            clickView.performClick();
+//            clickView.performClick();
+            gifView.release();
+            gifView.setVisibility(View.GONE);
         }
     }
 }
