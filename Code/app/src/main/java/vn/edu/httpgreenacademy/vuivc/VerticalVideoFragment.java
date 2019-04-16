5cf42380d79893a190481e92c5e4fac5394b4583
Tran Thi Bich Hong <tranthibichhong90proweb@gmail.com>
2019-04-16 18:44:41 +0700

    ModifiedVideoModel

--- Code/app/src/main/java/vn/edu/httpgreenacademy/vuivc/VerticalVideoFragment.java
+++ Code/app/src/main/java/vn/edu/httpgreenacademy/vuivc/VerticalVideoFragment.java
@@ -23,12 +23,12 @@ public class VerticalVideoFragment extends Fragment {
 
         verticalviewpager = viewVerticalViewPager.findViewById(R.id.verticalviewpager);
         listVideo.clear();
-        listVideo.add(new VideoModel("Nancy học tiếng mèo kêu","http://cdn13nofree.keeng.net/playnow/mp4/20190211/8BDE21AA601348BE.mp4",VideoTypeEnum.Normal));
-        listVideo.add(new VideoModel("Quàng thượng đáng yêu","http://cdn13nofree.keeng.net/uservideo/playnow/mp4/20180927/3427E73326DA4EF8.mp4",VideoTypeEnum.Normal));
-        listVideo.add(new VideoModel("Quàng thượng đáng yêu","http://cdn13nofree.keeng.net/uservideo/playnow/mp4/20180927/3427E73326DA4EF8.mp4",VideoTypeEnum.Normal));
-        listVideo.add(new VideoModel("TIKTOK","http://cdn13nofree.keeng.net/uservideo/playnow/mp4/20180619/FB7463.mp4",VideoTypeEnum.Normal));
-        listVideo.add(new VideoModel("Những tình huống hài hước trong Liên Quân","http://cdn13nofree.keeng.net/uservideo/playnow/mp4/20181001/8EF727.mp4",VideoTypeEnum.Normal));
-        listVideo.add(new VideoModel("Thắng bại tại Liên Quân","http://cdn13nofree.keeng.net/uservideo/playnow/mp4/20190222/9832f3ef86ab47fa8d100075162377fa.mp4",VideoTypeEnum.Normal));
+        listVideo.add(new VideoModel("Nancy học tiếng mèo kêu","http://cdn13nofree.keeng.net/playnow/mp4/20190211/8BDE21AA601348BE.mp4",VideoTypeEnum.Normal,1,1,1));
+        listVideo.add(new VideoModel("Quàng thượng đáng yêu","http://cdn13nofree.keeng.net/uservideo/playnow/mp4/20180927/3427E73326DA4EF8.mp4",VideoTypeEnum.Normal,1,1,1));
+        listVideo.add(new VideoModel("Quàng thượng đáng yêu","http://cdn13nofree.keeng.net/uservideo/playnow/mp4/20180927/3427E73326DA4EF8.mp4",VideoTypeEnum.Normal,1,1,1));
+        listVideo.add(new VideoModel("TIKTOK","http://cdn13nofree.keeng.net/uservideo/playnow/mp4/20180619/FB7463.mp4",VideoTypeEnum.Normal,1,1,1));
+        listVideo.add(new VideoModel("Những tình huống hài hước trong Liên Quân","http://cdn13nofree.keeng.net/uservideo/playnow/mp4/20181001/8EF727.mp4",VideoTypeEnum.Normal,1,1,1));
+        listVideo.add(new VideoModel("Thắng bại tại Liên Quân","http://cdn13nofree.keeng.net/uservideo/playnow/mp4/20190222/9832f3ef86ab47fa8d100075162377fa.mp4",VideoTypeEnum.Normal,1,1,1));
         verticalPagerAdapter = new VerticalPagerAdapter(getFragmentManager(),listVideo);
         verticalviewpager.setAdapter(verticalPagerAdapter);
         return viewVerticalViewPager;
--- Code/app/src/main/java/vn/edu/httpgreenacademy/vuivc/VideoModel.java
+++ Code/app/src/main/java/vn/edu/httpgreenacademy/vuivc/VideoModel.java
@@ -4,11 +4,15 @@ public class VideoModel {
     String VideoName;
     String VideoUrl;
     VideoTypeEnum videoType;
+    int NumberOfLike, NumberOfShare, NumberOfComment;
 
-    public VideoModel(String videoName, String videoUrl, VideoTypeEnum videoType) {
-        this.VideoName = videoName;
-        this.VideoUrl = videoUrl;
+    public VideoModel(String videoName, String videoUrl, VideoTypeEnum videoType, int numberOfLike, int numberOfShare, int numberOfComment) {
+        VideoName = videoName;
+        VideoUrl = videoUrl;
         this.videoType = videoType;
+        NumberOfLike = numberOfLike;
+        NumberOfShare = numberOfShare;
+        NumberOfComment = numberOfComment;
     }
 
     public String getVideoName() {
@@ -33,5 +37,29 @@ public class VideoModel {
 
     public void setVideoType(VideoTypeEnum videoType) {
         this.videoType = videoType;
+    }
+
+    public int getNumberOfLike() {
+        return NumberOfLike;
+    }
+
+    public void setNumberOfLike(int numberOfLike) {
+        NumberOfLike = numberOfLike;
+    }
+
+    public int getNumberOfShare() {
+        return NumberOfShare;
+    }
+
+    public void setNumberOfShare(int numberOfShare) {
+        NumberOfShare = numberOfShare;
+    }
+
+    public int getNumberOfComment() {
+        return NumberOfComment;
+    }
+
+    public void setNumberOfComment(int numberOfComment) {
+        NumberOfComment = numberOfComment;
     }
 }
