9db17d66c7ca6e32a537c35c6ee70239d136fb79
LuongChanHong <hongmainmail@gmail.com>
2019-04-18 20:49:56 +0700

    commit 8:49

--- Code/app/src/main/java/vn/edu/httpgreenacademy/vuivc/MainActivity.java
+++ Code/app/src/main/java/vn/edu/httpgreenacademy/vuivc/MainActivity.java
@@ -6,17 +6,32 @@ import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
+import vn.edu.httpgreenacademy.vuivc.Fragment.Comment.Comment_Dialog_Fragmment;
 import vn.edu.httpgreenacademy.vuivc.Fragment.ProfileUser.FragmentUserMain;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    ImageView imageViewComment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        imageViewComment = findViewById(R.id.imageViewComment);
+
+        imageViewComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Comment_Dialog_Fragmment comment_dialog_fragmment = new Comment_Dialog_Fragmment();
+                comment_dialog_fragmment.show(fragmentManager,"");
+                imageViewComment.setVisibility(View.INVISIBLE);
+            }
+        });
         //FragmentManager fragmentManager = getSupportFragmentManager();
         //FragmentUserMain fragmentUserMain = new FragmentUserMain();
         //fragmentManager.beginTransaction().replace(android.R.id.content, fragmentUserMain).commit();
