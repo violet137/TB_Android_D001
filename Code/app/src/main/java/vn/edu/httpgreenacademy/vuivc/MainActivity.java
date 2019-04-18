1465334fc87fcba83a0d078f32a85d045dbee988
LuongChanHong <hongmainmail@gmail.com>
2019-04-18 19:44:39 +0700

    no message

--- Code/app/src/main/java/vn/edu/httpgreenacademy/vuivc/MainActivity.java
+++ Code/app/src/main/java/vn/edu/httpgreenacademy/vuivc/MainActivity.java
@@ -9,11 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String myname = "My NAME: CƯỜNG" + "\n" + "Class: Android";
-        //phong
 
-        
-        String duy = "abcnd";
 
 
     }
