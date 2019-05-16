b3a5c9838cf1c0f93f5b3b9b3797511e11322567
NPCUONG1996 <npcuong1996@gmail.com>
2019-05-16 21:00:52 +0700

    Revert "Register mới"
    This reverts commit 2aa1bfddb721db176a4913a937c715bf1331d0d8.

--- Code/app/src/main/java/vn/edu/httpgreenacademy/vuivc/Utils/Interface/Register_Statis.java
+++ Code/app/src/main/java/vn/edu/httpgreenacademy/vuivc/Utils/Interface/Register_Statis.java
@@ -1,5 +1,5 @@
 package vn.edu.httpgreenacademy.vuivc.Utils.Interface;
 
 public interface Register_Statis {
-    void RegisterStatus(Boolean isThanhCong);
+    void RegisterStatus(Boolean isThanhCong); 
 }
